990c3b2dbbe31079f531c26ee57d0ab2df6ad41c
Daniel Silva <danieltdasilva@outlook.com>
2023-08-15 00:01:25 -0400

    CampsiteDetailPage with useParams

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import './App.css';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='contact' element={<ContactPage />} />
         <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
       </Routes>
       <Footer />
     </div>
   );
-}
+};
+
 
 export default App;
